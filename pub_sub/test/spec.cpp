fc78c50ffcb3f636213a879570bb57494bdf25fc
martin <martingerwallner@gmx.de>
2019-02-24 11:32:32 +0100

    Pub sub example: enable callbacks

--- pub_sub/test/spec.cpp
+++ pub_sub/test/spec.cpp
@@ -18,10 +18,15 @@ class IPublisher{
 
 class SubscriberImp : public ISubscriber{
   public:
+    using Callback = std::function<void(std::int32_t)>;
     SubscriberImp(std::weak_ptr<IPublisher> publisher) : publisher_(publisher){
     }
+    void SetReceiveCallback(Callback callback){
+      callback_ = callback;
+    }
+
     void Receive(std::int32_t data) override{
-      data_ = data;
+      callback_(data);
     }
     void RegisterMe(std::weak_ptr<SubscriberImp> me){
       if(publisher_.expired())
@@ -31,20 +36,21 @@ class SubscriberImp : public ISubscriber{
       auto pub = publisher_.lock();
       pub->Register(me);
     }
-    std::int32_t data_{};
   private:
     std::weak_ptr<IPublisher> publisher_;
+    Callback callback_{[](std::int32_t){}};
 };
 
-class Subscriber : public ISubscriber{
+class Subscriber{
   public:
-    Subscriber(std::weak_ptr<IPublisher> publisher) : impl(std::make_shared<SubscriberImp>(publisher)){
+    using Callback = std::function<void(std::int32_t)>;
+    Subscriber(std::weak_ptr<IPublisher> publisher, Callback callback) : impl(std::make_shared<SubscriberImp>(publisher)){
       impl->RegisterMe(impl);
+      impl->SetReceiveCallback(callback);
     }
-    void Receive(std::int32_t data){};
 
-    std::shared_ptr<SubscriberImp> impl;
   private:
+    std::shared_ptr<SubscriberImp> impl; // Created once and pointer passed around, when instance of this is copied or moved.
 };
 
 class Publisher : public IPublisher{
@@ -62,55 +68,62 @@ class Publisher : public IPublisher{
     std::weak_ptr<ISubscriber> subscriber_;
 };
 
-TEST(PubSubSpec, WhenSendingInt)
+class PubSubSpecFixture : public Test{
+  protected:
+    std::int32_t actual_data_{};
+    bool received_data_{false};
+    Subscriber::Callback callback_{[this](std::int32_t data){
+      received_data_ = true;
+      actual_data_ = data;
+    }};
+};
+TEST_F(PubSubSpecFixture, WhenSendingInt)
 {
   std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
-  Subscriber subscriber{publisher};
+  Subscriber subscriber{publisher, callback_};
   publisher->Send(3);
-  EXPECT_THAT(subscriber.impl->data_, Eq(3));
+  ASSERT_THAT(received_data_, Eq(true));
+  EXPECT_THAT(actual_data_, Eq(3));
 }
-TEST(PubSubSpec, WhenMovingSubsciber)
+TEST_F(PubSubSpecFixture, WhenMovingSubsciber)
 {
   std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
-  Subscriber subscriber(publisher);
+  Subscriber subscriber{publisher, callback_};
   Subscriber subscriber2(std::move(subscriber));
   publisher->Send(3);
-  EXPECT_THAT(subscriber2.impl->data_, Eq(3));
+  ASSERT_THAT(received_data_, Eq(true));
+  EXPECT_THAT(actual_data_, Eq(3));
 }
-TEST(PubSubSpec, WhenCopyingSubsciber)
+TEST_F(PubSubSpecFixture, WhenCopyingSubsciber)
 {
   std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
   std::shared_ptr<Publisher> publisher2 = std::make_shared<Publisher>();
-  Subscriber subscriber(publisher);
+  Subscriber subscriber(publisher, callback_);
   // Both subscriber have a pointer to the same impl instance. 
   // This is ok, as long as it is not possible to change the impl state after creation e. g. direct to a different publisher.
   Subscriber subscriber2(subscriber); 
   publisher->Send(3);
-  EXPECT_THAT(subscriber2.impl->data_, Eq(3));
+  ASSERT_THAT(received_data_, Eq(true));
+  EXPECT_THAT(actual_data_, Eq(3));
 }
-TEST(PubSubSpec, WhenCopyAssignmentOfSubsciber)
+TEST_F(PubSubSpecFixture, WhenCopyAssignmentOfSubsciber)
 {
   std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
   std::shared_ptr<Publisher> publisher2 = std::make_shared<Publisher>();
-  Subscriber subscriber(publisher);
+  Subscriber subscriber(publisher, callback_);
   // Both subscriber have a pointer to the same impl instance. 
   // This is ok, as long as it is not possible to change the impl state after creation e. g. direct to a different publisher.
   Subscriber subscriber2 = subscriber; 
   publisher->Send(3);
-  EXPECT_THAT(subscriber2.impl->data_, Eq(3));
+  ASSERT_THAT(received_data_, Eq(true));
+  EXPECT_THAT(actual_data_, Eq(3));
 }
-TEST(PubSubSpec, WhenSubscriberDestructedBeforeSend)
+TEST_F(PubSubSpecFixture, WhenSubscriberDestructedBeforeSend)
 {
   std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
   {
-    Subscriber subscriber(publisher);
+    Subscriber subscriber(publisher, callback_);
   }
   publisher->Send(3);
-}
-
-int main(int argc, char** argv) {
-  // The following line must be executed to initialize Google Mock
-  // (and Google Test) before running the tests.
-  ::testing::InitGoogleMock(&argc, argv);
-  return RUN_ALL_TESTS();
+  ASSERT_THAT(received_data_, Eq(false));
 }
