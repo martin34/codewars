f5f0b098a0009a50108dfcd1bd94ce6064b71529
martin <martingerwallner@gmx.de>
2019-02-24 10:55:51 +0100

    Imporve pub sub example

--- pub_sub/test/spec.cpp
+++ pub_sub/test/spec.cpp
@@ -12,71 +12,98 @@ class ISubscriber{
 class IPublisher{
   public:
     virtual void Send(std::int32_t data)=0;
-    virtual void Register(ISubscriber* subscriber)=0;
+    virtual void Register(std::weak_ptr<ISubscriber> subscriber)=0;
     virtual ~IPublisher(){}
 };
 
-class SimpleSubscriber : public ISubscriber{
+class SubscriberImp : public ISubscriber{
   public:
-    SimpleSubscriber(std::weak_ptr<IPublisher> publisher) : publisher_(publisher){
-      RegisterMe();
-    }
-    SimpleSubscriber(SimpleSubscriber&& donator){
-      this->publisher_ = donator.publisher_.lock();
-      RegisterMe();
-    }
-    ~SimpleSubscriber()
-    {
-      // Implement unsubscribe
+    SubscriberImp(std::weak_ptr<IPublisher> publisher) : publisher_(publisher){
     }
     void Receive(std::int32_t data) override{
       data_ = data;
     }
-  std::int32_t data_{};
-  private:
-    void RegisterMe(){
+    void RegisterMe(std::weak_ptr<SubscriberImp> me){
       if(publisher_.expired())
       {
         return;
       }
       auto pub = publisher_.lock();
-      pub->Register(this);
+      pub->Register(me);
     }
+    std::int32_t data_{};
+  private:
     std::weak_ptr<IPublisher> publisher_;
+};
+
+class Subscriber : public ISubscriber{
+  public:
+    Subscriber(std::weak_ptr<IPublisher> publisher) : impl(std::make_shared<SubscriberImp>(publisher)){
+      impl->RegisterMe(impl);
+    }
+    void Receive(std::int32_t data){};
+
+    std::shared_ptr<SubscriberImp> impl;
+  private:
 };
 
 class Publisher : public IPublisher{
   public:
     void Send(std::int32_t data)override{
-      subscriber_->Receive(data);
+      if(!subscriber_.expired())
+      {
+        subscriber_.lock()->Receive(data);
+      }
     }
-    void Register(ISubscriber* subscriber)override{
+    void Register(std::weak_ptr<ISubscriber> subscriber)override{
       subscriber_ = subscriber;
     } 
   private:
-    ISubscriber* subscriber_;
+    std::weak_ptr<ISubscriber> subscriber_;
 };
 
 TEST(PubSubSpec, WhenSendingInt)
 {
   std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
-  SimpleSubscriber subscriber(publisher);
+  Subscriber subscriber{publisher};
   publisher->Send(3);
-  EXPECT_THAT(subscriber.data_, Eq(3));
+  EXPECT_THAT(subscriber.impl->data_, Eq(3));
 }
 TEST(PubSubSpec, WhenMovingSubsciber)
 {
   std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
-  SimpleSubscriber subscriber(publisher);
-  SimpleSubscriber subscriber2(std::move(subscriber)); // Pointer in Publisher still points to old subscriber
+  Subscriber subscriber(publisher);
+  Subscriber subscriber2(std::move(subscriber));
   publisher->Send(3);
-  EXPECT_THAT(subscriber2.data_, Eq(3));
+  EXPECT_THAT(subscriber2.impl->data_, Eq(3));
 }
-TEST(PubSubSpec, DISABLED_WhenSubscriberDestructedBeforeSend)
+TEST(PubSubSpec, WhenCopyingSubsciber)
+{
+  std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
+  std::shared_ptr<Publisher> publisher2 = std::make_shared<Publisher>();
+  Subscriber subscriber(publisher);
+  // Both subscriber have a pointer to the same impl instance. 
+  // This is ok, as long as it is not possible to change the impl state after creation e. g. direct to a different publisher.
+  Subscriber subscriber2(subscriber); 
+  publisher->Send(3);
+  EXPECT_THAT(subscriber2.impl->data_, Eq(3));
+}
+TEST(PubSubSpec, WhenCopyAssignmentOfSubsciber)
+{
+  std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
+  std::shared_ptr<Publisher> publisher2 = std::make_shared<Publisher>();
+  Subscriber subscriber(publisher);
+  // Both subscriber have a pointer to the same impl instance. 
+  // This is ok, as long as it is not possible to change the impl state after creation e. g. direct to a different publisher.
+  Subscriber subscriber2 = subscriber; 
+  publisher->Send(3);
+  EXPECT_THAT(subscriber2.impl->data_, Eq(3));
+}
+TEST(PubSubSpec, WhenSubscriberDestructedBeforeSend)
 {
   std::shared_ptr<Publisher> publisher = std::make_shared<Publisher>();
   {
-    SimpleSubscriber subscriber(publisher);
+    Subscriber subscriber(publisher);
   }
   publisher->Send(3);
 }
