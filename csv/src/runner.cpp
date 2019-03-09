4d371dc52baeba3ef59c978f2e03457aebb134b2
martin <martingerwallner@gmx.de>
2019-03-09 15:02:54 +0100

    Refactor

--- csv/src/runner.cpp
+++ csv/src/runner.cpp
@@ -1,8 +1,4 @@
 #include "runner.h"
-#include "csv_file.h"
-#include  <filesystem>
-
-namespace fs = std::filesystem;
 
 const std::string Runner::ErrorStrings::invalid_args{"Not enough input arguments"};
 const std::string Runner::ErrorStrings::no_input_file{"Input file missing"};
@@ -20,13 +16,23 @@ std::vector<std::string> ConvertToStrings(int argc, char** argv)
 }
 bool ArgumentsValid(std::vector<std::string> const& arguments)
 {
-  return arguments.size() != 5;
+  return arguments.size() == 5;
 }
 
 Runner::Runner(std::vector<std::string> arguments, std::ostream & output_stream) : arguments_(arguments), output_stream_(output_stream){
 }
 void Runner::Do(){
-  if(ArgumentsValid(arguments_))
+  Prepare();
+  if(input_file_ && output_file_path_)
+  {
+    auto lines = input_file_.value().GetLines();
+    auto new_lines = CreateCopyWithReplacedColumn(lines, arguments_[2], arguments_[3]);
+    WriteLines(output_file_path_.value(), new_lines);
+  }
+}
+void Runner::Prepare()
+{
+  if(!ArgumentsValid(arguments_))
   {
     output_stream_ << ErrorStrings::invalid_args;
     return;
@@ -43,13 +49,12 @@ void Runner::Do(){
     output_stream_ << ErrorStrings::empty_input_file;
     return;
   }
+  input_file_ = std::move(csv);
   auto output_file_path = fs::absolute(fs::path{arguments_[4]});
   if(fs::exists(output_file_path))
   {
     output_stream_ << ErrorStrings::output_exits;
     return;
   }
-  auto lines = csv.GetLines();
-  auto new_lines = CreateCopyWithReplacedColumn(lines, arguments_[2], arguments_[3]);
-  WriteLines(output_file_path, new_lines);
+  output_file_path_ = output_file_path;
 }
--- csv/src/runner.h
+++ csv/src/runner.h
@@ -3,6 +3,11 @@
 
 #include <string>
 #include <vector>
+#include <filesystem>
+#include <optional>
+#include "csv/src/csv_file.h"
+
+namespace fs = std::filesystem;
 
 std::vector<std::string> ConvertToStrings(int argc, char** argv);
 
@@ -22,6 +27,10 @@ class Runner{
     std::vector<std::string> arguments_;
     std::ostream & output_stream_;
 
+    std::optional<CsvFileReader> input_file_;
+    std::optional<fs::path> output_file_path_;
+
+    void Prepare();
 };
 
 #endif // CSV_SRC_RUNNER_H_
