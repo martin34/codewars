8a254cdb681601bb8e379faa1ff0ead20a61f751
martin <martingerwallner@gmx.de>
2019-06-06 22:27:55 +0200

    Add some security infos

---
+++ security/README.md
@@ -0,0 +1,13 @@
+## Resources
+[C Memory Layout](https://www.geeksforgeeks.org/memory-layout-of-c-program/)
+[C Vulnerbilities](https://security.web.cern.ch/security/recommendations/en/codetools/c.shtml)
+[C++ Vulnerbilities](https://security.web.cern.ch/security/recommendations/en/codetools/cpp.shtml)
+* File Handles 
+* Buffer overflow
+* Never use malloc and free (instead new and delete)
+* Never use C style arrays and functions like fscanf
+[How buffer overflows work](http://arstechnica.com/security/2015/08/how-security-flaws-work-the-buffer-overflow/)
+[PC Assembly Language](http://www.drpaulcarter.com/pcasm/)
+[Smashing the Stack for Fun and Profit](http://insecure.org/stf/smashstack.html)
+[Exploiting Format String Vulnerabilities](http://crypto.stanford.edu/cs155/papers/formatstring-1.2.pdf)
+[Basic Integer Overflows](http://phrack.org/issues/60/10.html)
---
+++ security/buffer_overflow.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <stdio.h>
+
+void Authenticate(char* key)
+{
+  // strcopy will override authenticated. The program flow is changed.
+  int authenticated = 0;
+  char buffer[4];
+  strcpy(buffer, key);
+  if(authenticated)
+  {
+    printf("buffer overflow");
+  }
+}
+
+
+int main()
+{
+  char* key = "AuthMe";
+  Authenticate(key);
+  return 0;
+}
+
+/* 
+ * Buffer overflows can be used to execute injected code, modify data, reveal secret data
+ * 1. Inject machine code (e. g. via a input file)
+ * 2. Trigger an overflow and override the return address saved on the stack. The new value points to the injected code.
+ * --> Injected code is executed --> e. g. shell can be opened and the attacker has access to the system.
+ *
+ * It is difficult to know the exact location of the injected code and therefore to override the return pointer with the right value. 
+ * Especially if you don't know the source code. 
+ * Injecting a lot of nop at the beginning of the code and with the assumtion that only a small part of the stack is used, a trial and error approach can be used. 
+ *
+ * Heap overflow is a variation
+ * This is also possible in C++ e. g. overrriding the vtable of an object (inheritance hirachy), because of an overflow in an member variable.
+ *
+ * Integer overflow
+ * Can lead to buffer overflow
+ */
+
