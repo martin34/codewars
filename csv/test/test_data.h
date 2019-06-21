d7cb21509700ee8706121b220fec88ace3cc2fdb
martin <martingerwallner@gmx.de>
2019-06-21 15:32:05 +0200

    Clang format

--- csv/test/test_data.h
+++ csv/test/test_data.h
@@ -6,20 +6,20 @@
 
 namespace fs = std::filesystem;
 
-inline std::string GetEmptyCsvFilePathRelative()
-{
+inline std::string GetEmptyCsvFilePathRelative() {
   return "csv/test/data/empty_file.csv";
 }
-inline fs::path GetEmptyCsvFilePath(){
+inline fs::path GetEmptyCsvFilePath() {
   return fs::absolute(GetEmptyCsvFilePathRelative());
 }
-inline std::string GetShortCsvFilePathRelative(){
+inline std::string GetShortCsvFilePathRelative() {
   return "csv/test/data/short_file.csv";
 }
-inline fs::path GetShortCsvFilePath(){
+inline fs::path GetShortCsvFilePath() {
   return fs::absolute(GetShortCsvFilePathRelative());
 }
-inline std::vector<std::string> GetShortCsvContnet(){
-  return {{"column_0,column_1,column_2,column_3"}, {"w,x,y,z"}, {"11,12,12,14"}};
+inline std::vector<std::string> GetShortCsvContnet() {
+  return {
+      {"column_0,column_1,column_2,column_3"}, {"w,x,y,z"}, {"11,12,12,14"}};
 }
 #endif // CSV_TEST_TEST_DATA_H_
