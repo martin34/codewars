10f166602afa005f7b24b02927f9f422b8868274
martin <martingerwallner@gmx.de>
2019-07-23 21:52:36 +0200

    Simple cpp polymorphism example

---
+++ simple_cpp/test/some_class_spec.cpp
@@ -0,0 +1,31 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "simple_cpp/src/some_class.h"
+
+using namespace ::testing;
+
+TEST(Parent, WhenTypical) {
+  Parent unit;
+  EXPECT_THAT(unit.ReturnANumber(), Eq(kParent));
+  EXPECT_THAT(unit.ReturnANumberFoo(), Eq(kParent));
+}
+
+TEST(Child, WhenTypical) {
+  Child unit;
+  EXPECT_THAT(unit.ReturnANumber(), Eq(kChild));
+  EXPECT_THAT(unit.ReturnANumberFoo(), Eq(kChild));
+}
+
+TEST(Child, WhenUsingParentPointer) {
+  std::unique_ptr<Parent> unit = std::make_unique<Child>();
+  EXPECT_THAT(unit->ReturnANumber(), Eq(kChild));
+  EXPECT_THAT(unit->ReturnANumberFoo(), Eq(kChild));
+}
+
+TEST(Child, WhenUsingParentReference) {
+  Child unit;
+  Parent &ref_unit = unit;
+  EXPECT_THAT(ref_unit.ReturnANumber(), Eq(kChild));
+  EXPECT_THAT(unit.ReturnANumberFoo(), Eq(kChild));
+}
