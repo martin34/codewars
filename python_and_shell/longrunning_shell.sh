faeec10eee2b6a2ae4056d330d9985eded12df5b
martin <martingerwallner@gmx.de>
2019-07-31 22:46:11 +0200

    Python: How to wrap shell code

---
+++ python_and_shell/longrunning_shell.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for i in 1 2 3 4 5 6 7 8 9
+do
+  echo "Loop executed $i times"
+  sleep 1
+done
---
+++ python_and_shell/python_wrapper.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import subprocess
+
+cmd = ['./longrunning_shell.sh']
+
+def execute_blocking():
+  print("Execute script blocking")
+  process = subprocess.run(cmd, stdout=subprocess.PIPE)
+  print("stdout of shell script: {}".format(process))
+
+def execute_non_blocking():
+  print("Execute non blocking")
+  with subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True) as process:
+      return_code = None
+      while return_code is None:
+          print("Read from stdout: {}".format(process.stdout.readline())) # Be careful to use readline and not readlines
+          return_code = process.poll()
+      print("Return code {}".format(return_code))
+
+
+def main():
+  execute_blocking()
+  execute_non_blocking()
+
+if __name__== "__main__":
+  main()
+
+
