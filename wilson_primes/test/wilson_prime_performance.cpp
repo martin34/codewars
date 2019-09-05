ef4e16870c2e568f9fc93c7e3ecd88b5e624771a
martin <martingerwallner@gmx.de>
2019-09-05 21:48:09 +0200

    Benchmark: Improve input for IsPrime Benchmark

--- wilson_primes/test/wilson_prime_performance.cpp
+++ wilson_primes/test/wilson_prime_performance.cpp
@@ -1,4 +1,6 @@
 #include <benchmark/benchmark.h>
+#include <iostream>
+#include <random>
 
 #include "wilson_primes/src/wilson_prime.h"
 
@@ -10,6 +12,18 @@ static void BM_IsPrime(benchmark::State &state) {
   }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_IsPrime)->RangeMultiplier(2)->Range(2, 1'000'000'000);
+// BENCHMARK(BM_IsPrime)->RangeMultiplier(2)->Range(2, 1'000'000'000);
+
+static void CustomArguments(benchmark::internal::Benchmark *b) {
+  std::int32_t seed{15};
+  std::mt19937 gen(seed);
+  std::uniform_int_distribution<> dis(2, 1'000'000'000);
+  for (std::int32_t i = 0; i < 100; ++i) {
+    auto arg = dis(gen);
+    b->Args({arg});
+  }
+}
+BENCHMARK(BM_IsPrime)->Apply(CustomArguments);
+
 // Run the benchmark
 BENCHMARK_MAIN();
