a6847ed095e2a856590187fcc1f2282c6e41648a
martin <martingerwallner@gmx.de>
2019-09-10 21:36:49 +0200

    Pocker: Implement Pair, ThreeOfAKind and FourOfAKind

--- pocker/test/pocker_spec.cpp
+++ pocker/test/pocker_spec.cpp
@@ -1,102 +1,51 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-enum FaceValue {
-  Two,
-  Three,
-  Four,
-  Five,
-  Six,
-  Seven,
-  Eight,
-  Nine,
-  Ten,
-  Jack,
-  Queen,
-  King,
-  Ace
-};
+#include "pocker.h"
 
-enum Suit { Heart, Tile, Clover, Pike };
-
-struct Card {
-  Card(Suit suit, FaceValue face_value) : suit(suit), face_value(face_value) {}
-  Suit suit;
-  FaceValue face_value;
-};
-
-enum Score {
-  HighCard,
-  OnePair,
-  TwoPairs,
-  ThreeOfAKind,
-  Straight,
-  Flush,
-  FullHouse,
-  FourOfAKind,
-  StraightFlush,
-  RoyalFlush
-};
-
-class Hand {
-public:
-  Hand(Card c0, Card c1, Card c2, Card c3, Card c4)
-      : hand{c0, c1, c2, c3, c4} {}
-
-  Score GetMostValuableScore() const {
-    for (auto const &card : hand) {
-      int count{};
-      for (auto const &others : hand) {
-        if (card.face_value == others.face_value) {
-          ++count;
-        }
-      }
-      if (count == 2) {
-        return OnePair;
-      }
-    }
-    return HighCard;
-  }
-
-private:
-  std::vector<Card> hand;
-};
-
-// Shall contain simple logic like
-// lhs.MostValuableScore() -> TwoPairs/Pair/Flush/...
-// lhs.GetHighestCard()
-bool operator<(const Hand &lhs, const Hand &rhs) {
-  return lhs.GetMostValuableScore() < rhs.GetMostValuableScore();
-}
-bool operator>(const Hand &lhs, const Hand &rhs) { return !(lhs < rhs); }
-// bool operator==(const Hand &lhs, const Hand &rhs) {
-//   return lhs < rhs;
-// }
 namespace {
 
 using namespace testing;
+using namespace pocker;
 
+TEST(PlayerScoreSpec, WhenSameHand) {
+  Hand one{
+      {Pike, Two}, {Tile, Six}, {Clover, Three}, {Pike, Ace}, {Heart, Four}};
+  Hand two{
+      {Heart, Two}, {Tile, Six}, {Clover, Three}, {Pike, Ace}, {Heart, Four}};
+  EXPECT_THAT(one, Eq(two));
+}
 TEST(PlayerScoreSpec, WhenPairs) {
   Hand one{
-      {Heart, Two}, {Tile, Two}, {Clover, Three}, {Pike, Ace}, {Heart, Four}};
+      {Pike, Two}, {Tile, Two}, {Clover, Three}, {Pike, Ace}, {Heart, Four}};
   Hand two{
       {Heart, Two}, {Tile, Six}, {Clover, Three}, {Pike, Ace}, {Heart, Four}};
   EXPECT_THAT(one, Gt(two));
 }
-TEST(PlayerScoreSpec, WhenPairsReversed) {
-  Hand one{
-      {Heart, Two}, {Tile, Six}, {Clover, Three}, {Pike, Ace}, {Heart, Four}};
-  Hand two{
-      {Heart, Two}, {Tile, Two}, {Clover, Three}, {Pike, Ace}, {Heart, Four}};
-  EXPECT_THAT(one, Lt(two));
+
+class PlayerScoreSpecParameterized
+    : public TestWithParam<std::pair<Hand, Hand>> {};
+
+TEST_P(PlayerScoreSpecParameterized, WhenHandOneLoses) {
+  EXPECT_THAT(GetParam().first, Lt(GetParam().second));
 }
-// TEST(PlayerScoreSpec, WhenSameHand) {
-//   Hand one{
-//       {Heart, Two}, {Tile, Six}, {Clover, Three}, {Pike, Ace}, {Heart,
-//       Four}};
-//   Hand two{
-//       {Heart, Two}, {Tile, Six}, {Clover, Three}, {Pike, Ace}, {Heart,
-//       Four}};
-//   EXPECT_THAT(one, Eq(two));
-// }
+
+INSTANTIATE_TEST_CASE_P(_, PlayerScoreSpecParameterized,
+                        Values(
+                            // clang-format off
+      // WhenPairsReversed
+      std::pair<Hand, Hand> {
+      {{Pike, Two}, {Tile, Six}, {Clover, Three}, {Pike, Ace}, {Heart, Four}},
+      {{Heart, Two}, {Tile, Two}, {Heart, Three},{Pike, Ace}, {Heart, Four}}},
+      // WhenPairsAndThreeOfAKind
+      std::pair<Hand, Hand> {
+      {{Pike, Two}, {Tile, Two}, {Clover, Three}, {Pike, Ace}, {Heart, Four}},
+      {{Heart, Two}, {Tile, Six}, {Clover, Six}, {Pike, Six}, {Heart, Four}}},
+      // WhenThreeOfAKindAndFourOfAKind
+      std::pair<Hand, Hand> {
+      {{Heart, Three}, {Tile, Six}, {Clover, Six}, {Pike, Six}, {Heart, Four}},
+      {{Pike, Two}, {Tile, Two}, {Clover, Three}, {Heart, Two}, {Clover, Two}}}
+                            // clang-format on
+                            ));
+
 } // namespace
