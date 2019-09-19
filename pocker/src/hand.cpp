1a05946686d88779b31e86044e09f89ac923c4a6
martin <martingerwallner@gmx.de>
2019-09-19 22:26:50 +0200

    Pocker: Extend impl

--- pocker/src/hand.cpp
+++ pocker/src/hand.cpp
@@ -5,6 +5,8 @@
 
 namespace pocker {
 
+using FaceValueTypeVector = std::vector<std::underlying_type<FaceValue>::type>;
+
 Hand::Hand(Card c0, Card c1, Card c2, Card c3, Card c4)
     : hand{c0, c1, c2, c3, c4} {
   std::sort(hand.begin(), hand.end(), [](Card const &lhs, Card const &rhs) {
@@ -12,27 +14,63 @@ Hand::Hand(Card c0, Card c1, Card c2, Card c3, Card c4)
   });
 }
 
+std::optional<Score> GetStraight(FaceValueTypeVector const &diff,
+                                 std::vector<Card> const &hand) {
+  auto ones = std::count(std::next(diff.cbegin()), diff.cend(),
+                         std::underlying_type<FaceValue>::type{1});
+  if (ones == 4) {
+    return std::optional{Score{Score::Straight, hand.back()}};
+  }
+  return {};
+}
+std::optional<Score> GetFourOfAKind(std::int32_t zeros,
+                                    FaceValueTypeVector const &diff,
+                                    std::vector<Card> const &hand) {
+  if (zeros != 3) {
+    return {};
+  }
+  auto position_with_card = std::find(diff.cbegin(), diff.cend(), 0);
+  auto distance = std::distance(diff.cbegin(), position_with_card);
+  auto card = hand.cbegin();
+  std::advance(card, distance);
+  return std::optional{Score{Score::FourOfAKind, *card}};
+}
+std::optional<Score> GetThreeOfAKind(std::int32_t zeros,
+                                     FaceValueTypeVector const &diff,
+                                     std::vector<Card> const &hand) {
+  if (zeros != 2) {
+    return {};
+  }
+  auto position_with_card = std::find(diff.cbegin(), diff.cend(), 0);
+  auto distance = std::distance(diff.cbegin(), position_with_card);
+  auto card = hand.cbegin();
+  std::advance(card, distance);
+  return std::optional{Score{Score::ThreeOfAKind, *card}};
+}
 Score Hand::GetMostValuableScore() const {
-  std::vector<std::underlying_type<FaceValue>::type> face_values;
+  FaceValueTypeVector face_values;
   std::transform(hand.cbegin(), hand.cend(), std::back_inserter(face_values),
                  [](auto const &card) { return card.face_value; });
 
-  std::vector<std::underlying_type<FaceValue>::type> diff;
+  FaceValueTypeVector diff;
   std::adjacent_difference(face_values.cbegin(), face_values.cend(),
                            std::back_inserter(diff));
 
   auto zeros = std::count(std::next(diff.cbegin()), diff.cend(),
                           std::underlying_type<FaceValue>::type{0});
 
-  auto ones = std::count(std::next(diff.cbegin()), diff.cend(),
-                         std::underlying_type<FaceValue>::type{1});
-
-  if (zeros == 3)
-    return Score{Score::FourOfAKind};
-  if (ones == 4)
-    return Score{Score::Straight};
-  if (zeros == 2)
-    return Score{Score::ThreeOfAKind};
+  auto four_of_a_kind = GetFourOfAKind(zeros, diff, hand);
+  if (four_of_a_kind) {
+    return four_of_a_kind.value();
+  }
+  auto straight = GetStraight(diff, hand);
+  if (straight) {
+    return straight.value();
+  }
+  auto three_of_a_kind = GetThreeOfAKind(zeros, diff, hand);
+  if (three_of_a_kind) {
+    return three_of_a_kind.value();
+  }
   if (zeros == 1) {
     return Score{Score::OnePair, GetACardFromPair()};
   }
