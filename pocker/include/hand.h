0ffdb3a29f9dd5b79516eb04c972a47550c64261
martin <martingerwallner@gmx.de>
2019-09-23 14:06:03 +0200

    Pocker: Refactor

--- pocker/include/hand.h
+++ pocker/include/hand.h
@@ -9,14 +9,22 @@
 namespace pocker {
 class Hand {
 public:
+  using FaceValueType = std::underlying_type<FaceValue>::type;
+  using FaceValueTypeVector = std::vector<FaceValueType>;
   Hand(Card c0, Card c1, Card c2, Card c3, Card c4);
 
   Score GetMostValuableScore() const;
 
 private:
   Card GetACardFromPair() const;
+  std::optional<Score> GetPair() const;
+  std::optional<Score> GetTwoPairs() const;
+  std::optional<Score> GetThreeOfAKind() const;
+  std::optional<Score> GetStraight() const;
+  std::optional<Score> GetFourOfAKind() const;
 
-  std::vector<Card> hand;
+  std::vector<Card> hand_;
+  FaceValueTypeVector face_value_adjacent_diff_{};
   friend std::ostream &operator<<(std::ostream &os, const Hand &p);
 };
 
