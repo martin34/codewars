f7725d2ea5eec13c1ff438b29ed6bf59b870b348
martin <martingerwallner@gmx.de>
2019-09-25 20:59:52 +0200

    Pocker: Fix and refactor

--- pocker/src/hand.cpp
+++ pocker/src/hand.cpp
@@ -93,33 +93,25 @@ std::optional<Score> Hand::GetStraight() const {
 }
 
 std::optional<Score> Hand::GetFourOfAKind() const {
-  auto second_card_of_pair_positions = GetSecondCardOfPairPositions();
-  if (second_card_of_pair_positions.size() != 3) {
-    return {};
-  }
-  std::vector<FaceValueTypeVector::difference_type> distance_between_pairs;
-  auto first = second_card_of_pair_positions.cbegin();
-  auto second = std::next(second_card_of_pair_positions.cbegin());
-  for (; first != second_card_of_pair_positions.cend() &&
-         second != second_card_of_pair_positions.cend();
-       ++first, ++second) {
-    distance_between_pairs.push_back(std::distance(*first, *second));
-  }
-  if ((distance_between_pairs.front() != 1) ||
-      (distance_between_pairs.back() != 1)) {
+  auto const second_cards_of_pairs = GetSecondCardOfPairs();
+  if (second_cards_of_pairs.size() != 3) {
     return {};
   }
 
-  auto distance = std::distance(face_value_adjacent_diff_.cbegin(),
-                                second_card_of_pair_positions.front());
-  auto card = hand_.cbegin();
-  std::advance(card, distance);
-  return std::optional{Score{Score::FourOfAKind, *card}};
+  auto const third_card = std::next(second_cards_of_pairs.front());
+  auto const fourth_card = std::next(third_card);
+  if ((third_card != second_cards_of_pairs.at(1)) ||
+      (fourth_card != second_cards_of_pairs.at(2))) {
+    return {};
+  }
+
+  return std::optional{
+      Score{Score::FourOfAKind, *second_cards_of_pairs.front()}};
 }
 
 std::optional<Score> Hand::GetFullHouse() const {
-  auto second_card_of_pair_positions = GetSecondCardOfPairPositions();
-  if (second_card_of_pair_positions.size() != 3) {
+  auto second_cards_of_pairs = GetSecondCardOfPairs();
+  if (second_cards_of_pairs.size() != 3) {
     return {};
   }
   auto three_of_a_kind = GetThreeOfAKind();
@@ -169,66 +161,45 @@ std::optional<Score> Hand::GetThreeOfAKind() const {
   return std::optional{Score{Score::ThreeOfAKind, *card}};
 }
 
-Card GetCardNotPartOfPair(
-    std::vector<Card> const &hand,
-    FaceValueTypeVector::difference_type distance_two_second_high_pair_card,
-    FaceValueTypeVector::difference_type distance_two_second_low_pair_card) {
-  std::vector<bool> not_part_of_pair{true, true, true, true, true};
-  not_part_of_pair[distance_two_second_high_pair_card] = false;
-  not_part_of_pair[distance_two_second_high_pair_card - 1] = false;
-  not_part_of_pair[distance_two_second_low_pair_card] = false;
-  not_part_of_pair[distance_two_second_low_pair_card - 1] = false;
-  auto v = std::find(not_part_of_pair.cbegin(), not_part_of_pair.cend(), true);
-  auto dist = std::distance(not_part_of_pair.cbegin(), v);
-  return hand[dist];
-}
-
 std::optional<Score> Hand::GetTwoPairs() const {
   //   Value: 1 1  2  3  3 | 2  3 3  4 4 | 2 2  4 4  5
   //    Diff: 1 0 -1 -1 -0 | 2 -1 0 -1 0 | 2 0 -2 0 -1
   // Pattern:   0  x  x  0 |    x 0  x 0 |   0  x 0  x
 
-  auto zero_iterators = GetSecondCardOfPairPositions();
+  auto zero_iterators = GetSecondCardOfPairs();
   if (zero_iterators.size() != 2) {
     return {};
   }
-  auto distance = std::distance(zero_iterators.front(), zero_iterators.back());
-  if (distance == 1) {
+  if (std::next(zero_iterators.front()) == zero_iterators.back()) {
     return {};
   }
-  auto high_pair_card = hand_.cbegin();
-  auto distance_to_high_pair =
-      std::distance(face_value_adjacent_diff_.cbegin(), zero_iterators.back());
-  std::advance(high_pair_card, distance_to_high_pair);
-  auto low_pair_card = hand_.cbegin();
-  auto distance_to_low_pair =
-      std::distance(face_value_adjacent_diff_.cbegin(), zero_iterators.front());
-  std::advance(low_pair_card, distance_to_low_pair);
+  auto high_pair_card = *zero_iterators.back();
+  auto low_pair_card = *zero_iterators.front();
+  auto not_pair_cards = GetCardsNotPartOfPair(zero_iterators);
 
   Score score{Score::TwoPairs};
-  score.AddNextTieBreaker(*high_pair_card);
-  score.AddNextTieBreaker(*low_pair_card);
-  score.AddNextTieBreaker(
-      GetCardNotPartOfPair(hand_, distance_to_high_pair, distance_to_low_pair));
+  score.AddNextTieBreaker(high_pair_card);
+  score.AddNextTieBreaker(low_pair_card);
+  score.AddNextTieBreaker(not_pair_cards.front());
   return score;
 }
 
 std::optional<Score> Hand::GetPair() const {
-
-  auto zeros = std::count(std::next(face_value_adjacent_diff_.cbegin()),
-                          face_value_adjacent_diff_.cend(),
-                          std::underlying_type<FaceValue>::type{0});
-  if (zeros != 1) {
+  auto const zero_iterators = GetSecondCardOfPairs();
+  if (zero_iterators.size() != 1) {
     return {};
   }
-  auto position_with_card = std::find(face_value_adjacent_diff_.cbegin(),
-                                      face_value_adjacent_diff_.cend(), 0);
-  auto distance =
-      std::distance(face_value_adjacent_diff_.cbegin(), position_with_card);
-  auto card = hand_.cbegin();
-  std::advance(card, distance);
-  return std::optional{Score{Score::OnePair, *card}};
+  Score score{Score::OnePair};
+
+  score.AddNextTieBreaker(*zero_iterators.front());
+
+  auto non_pair_cards = GetCardsNotPartOfPair(zero_iterators);
+  for (auto it = non_pair_cards.crbegin(); it != non_pair_cards.crend(); ++it) {
+    score.AddNextTieBreaker(*it);
+  }
+  return std::optional{score};
 }
+
 std::ostream &operator<<(std::ostream &os, const Hand &obj) {
   for (auto const &o : obj.hand_) {
     os << o << "; ";
@@ -236,8 +207,8 @@ std::ostream &operator<<(std::ostream &os, const Hand &obj) {
   return os;
 }
 
-std::vector<FaceValueTypeVector::const_iterator>
-Hand::GetSecondCardOfPairPositions() const {
+std::vector<std::vector<Card>::const_iterator>
+Hand::GetSecondCardOfPairs() const {
   auto zero_it = std::next(face_value_adjacent_diff_.cbegin());
   std::vector<FaceValueTypeVector::const_iterator> zero_iterators;
   while (zero_it != face_value_adjacent_diff_.cend()) {
@@ -247,6 +218,40 @@ Hand::GetSecondCardOfPairPositions() const {
       zero_iterators.push_back(zero_it++);
     }
   }
-  return zero_iterators;
+  std::vector<std::vector<Card>::const_iterator> cards_part_of_pair;
+  for (auto const it : zero_iterators) {
+    cards_part_of_pair.push_back(GetCardPartOfPair(it));
+  }
+  return cards_part_of_pair;
+}
+
+std::vector<Card> Hand::GetCardsNotPartOfPair(
+    std::vector<std::vector<Card>::const_iterator> second_cards_of_pair) const {
+  std::vector<Card> non_par_cards;
+  std::vector<std::vector<Card>::const_iterator> first_card_of_pair;
+  for (auto const v : second_cards_of_pair) {
+    first_card_of_pair.push_back(std::prev(v));
+  }
+  std::vector<std::vector<Card>::const_iterator> cards_of_pairs(
+      second_cards_of_pair);
+  for (auto const v : first_card_of_pair) {
+    cards_of_pairs.push_back(v);
+  }
+  for (auto it = hand_.cbegin(); it != hand_.cend(); ++it) {
+    if (std::all_of(cards_of_pairs.cbegin(), cards_of_pairs.cend(),
+                    [it](auto const v) { return it != v; })) {
+      non_par_cards.push_back(*it);
+    }
+  }
+  return non_par_cards;
+}
+
+std::vector<Card>::const_iterator Hand::GetCardPartOfPair(
+    FaceValueTypeVector::const_iterator second_card_of_pair_position) const {
+  auto distance = std::distance(face_value_adjacent_diff_.cbegin(),
+                                second_card_of_pair_position);
+  auto card = hand_.cbegin();
+  std::advance(card, distance);
+  return card;
 }
 } // namespace pocker
