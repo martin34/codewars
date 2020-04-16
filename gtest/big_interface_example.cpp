ccdf6b4f8de88ef03e2854bee501ca80f80c990e
martin <martingerwallner@gmx.de>
2020-04-16 21:46:59 +0200

    Gtest example for testing class with big interface

---
+++ gtest/big_interface_example.cpp
@@ -0,0 +1,108 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include <string>
+
+namespace {
+
+using namespace ::testing;
+
+class Proxy {
+public:
+  void SetA(bool a) { a_ = a; }
+  void SetB(int b) { b_ = b; }
+  void SetC(double c) { c_ = c; }
+  void SetD(std::string d) { d_ = d; }
+  bool IsAspectX() const { return a_ && (c_ > b_); }
+  bool IsAspectY() const { return DContains("foo") && (c_ > b_); }
+  bool IsAspectZ() const { return DContains("bar") && a_; }
+
+private:
+  bool DContains(const std::string &v) const {
+    return d_.find(v) != std::string::npos;
+  }
+  bool a_{};
+  int b_{};
+  double c_{};
+  std::string d_{};
+};
+
+struct Params {
+  bool a;
+  int b;
+  double c;
+  std::string d;
+  bool aspect_x;
+  bool aspect_y;
+  bool aspect_z;
+};
+class ParamsBuilder {
+public:
+  Params Build() { return params_; }
+  ParamsBuilder &WithA(bool a) {
+    params_.a = a;
+    return *this;
+  }
+  ParamsBuilder &WithB(int b) {
+    params_.b = b;
+    return *this;
+  }
+  ParamsBuilder &WithC(double c) {
+    params_.c = c;
+    return *this;
+  }
+  ParamsBuilder &WithD(const std::string &d) {
+    params_.d = d;
+    return *this;
+  }
+  ParamsBuilder &ExpectX(bool x) {
+    params_.aspect_x = x;
+    return *this;
+  }
+  ParamsBuilder &ExpectY(bool y) {
+    params_.aspect_y = y;
+    return *this;
+  }
+  ParamsBuilder &ExpectZ(bool z) {
+    params_.aspect_z = z;
+    return *this;
+  }
+
+private:
+  Params params_;
+};
+
+class OtherTestFixture : public testing::TestWithParam<Params> {
+public:
+  OtherTestFixture() {
+    unit.SetA(GetParam().a);
+    unit.SetB(GetParam().b);
+    unit.SetC(GetParam().c);
+    unit.SetD(GetParam().d);
+  }
+  Proxy unit{};
+};
+
+TEST_P(OtherTestFixture, Aspects) {
+  EXPECT_THAT(unit.IsAspectX(), Eq(GetParam().aspect_x));
+  EXPECT_THAT(unit.IsAspectY(), Eq(GetParam().aspect_y));
+  EXPECT_THAT(unit.IsAspectZ(), Eq(GetParam().aspect_z));
+}
+
+INSTANTIATE_TEST_SUITE_P(Foo, OtherTestFixture,
+                         Values(
+                             // clang-format off
+    ParamsBuilder().WithA(true).WithB(1).WithC(5.).ExpectX(true).ExpectY(false).ExpectZ(false).Build(),
+    ParamsBuilder().WithA(false).WithB(1).WithC(5.).ExpectX(false).ExpectY(false).ExpectZ(false).Build(),
+    ParamsBuilder().WithA(true).WithB(5).WithC(1.).ExpectX(false).ExpectY(false).ExpectZ(false).Build(),
+    ParamsBuilder().WithA(true).WithD("bar").ExpectX(false).ExpectY(false).ExpectZ(true).Build(),
+    ParamsBuilder().WithA(true).WithD("BAR").ExpectX(false).ExpectY(false).ExpectZ(false).Build(),
+    ParamsBuilder().WithB(1).WithC(5.).WithD("foo").ExpectX(false).ExpectY(true).ExpectZ(false).Build(),
+    ParamsBuilder().WithB(1).WithC(5.).WithD("FOO").ExpectX(false).ExpectY(false).ExpectZ(false).Build(),
+    ParamsBuilder().WithB(5).WithC(1.).WithD("foo").ExpectX(false).ExpectY(false).ExpectZ(false).Build(),
+    ParamsBuilder().WithA(true).WithB(1).WithC(5.).WithD("bar").ExpectX(true).ExpectY(false).ExpectZ(true).Build(),
+    ParamsBuilder().WithA(true).WithB(1).WithC(5.).WithD("foobar").ExpectX(true).ExpectY(true).ExpectZ(true).Build()
+                             // clang-format on 
+    ));
+
+} // namespace
