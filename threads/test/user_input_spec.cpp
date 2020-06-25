aa04900901595d99014e81b29d9ff5dec6624a9a
martin <martingerwallner@gmx.de>
2020-06-25 21:30:59 +0200

    Use boost::process in test

--- threads/test/user_input_spec.cpp
+++ threads/test/user_input_spec.cpp
@@ -7,34 +7,32 @@
 #include <memory>
 #include <stdexcept>
 #include <string>
+#include <vector>
+
+#include <boost/process.hpp>
 
 namespace {
 using namespace ::testing;
 
-// To also provide stdin to the child we can use fork and pipe:
-// https://stackoverflow.com/questions/6743771/popen-alternative
-// or even better a process managing library e. g.
-// https://github.com/skystrife/procxx or boost::process
-// https://www.boost.org/doc/libs/1_64_0/doc/html/process.html
-std::string exec(const char *cmd) {
-  std::array<char, 128> buffer;
-  std::string result;
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-  if (!pipe) {
-    throw std::runtime_error("popen() failed!");
+std::vector<std::string> exec(const char *cmd) {
+  boost::process::ipstream pipe_stream;
+  boost::process::child c{cmd, boost::process::std_out > pipe_stream};
+
+  std::vector<std::string> lines{};
+
+  std::string line{};
+  while (pipe_stream && std::getline(pipe_stream, line) && !line.empty()) {
+    lines.push_back(line);
   }
-  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-    result += buffer.data();
-  }
-  return result;
+
+  return lines;
 }
 
 TEST(Foo, Bar) {
   const char *path{"./threads/user_input"};
   auto actual = exec(path);
 
-  std::cerr << "Got on stdin: " << actual;
-  EXPECT_THAT(actual, Eq(std::string{"Foo\n"}));
+  EXPECT_THAT(actual, Contains(std::string{"Foo"}));
 }
 
 } // namespace
