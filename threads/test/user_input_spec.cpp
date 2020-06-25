f8b90d5407c648bf8f4ffc0a5176993aacad6b0a
martin <martingerwallner@gmx.de>
2020-06-25 22:21:34 +0200

    Test with stdin and stdout with boost process

--- threads/test/user_input_spec.cpp
+++ threads/test/user_input_spec.cpp
@@ -1,11 +1,6 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-#include <array>
-#include <cstdio>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -14,25 +9,42 @@
 namespace {
 using namespace ::testing;
 
-std::vector<std::string> exec(const char *cmd) {
+std::vector<std::string> exec(const char *cmd, const std::string &stdin) {
   boost::process::ipstream pipe_stream;
-  boost::process::child c{cmd, boost::process::std_out > pipe_stream};
+  boost::process::opstream in;
+
+  boost::process::child c{cmd, boost::process::std_out > pipe_stream,
+                          boost::process::std_in < in};
+
+  in << stdin << std::endl;
 
   std::vector<std::string> lines{};
 
   std::string line{};
-  while (pipe_stream && std::getline(pipe_stream, line) && !line.empty()) {
+  if (pipe_stream && std::getline(pipe_stream, line) && !line.empty()) {
     lines.push_back(line);
   }
 
+  c.terminate();
   return lines;
 }
 
-TEST(Foo, Bar) {
+TEST(Multiprocess, CheckStdout) {
   const char *path{"./threads/user_input"};
-  auto actual = exec(path);
+  auto actual = exec(path, "");
 
   EXPECT_THAT(actual, Contains(std::string{"Foo"}));
 }
+TEST(Multiprocess, ReactOnStdinWhenBar) {
+  const char *path{"./threads/user_input --stdin2stdout Bar"};
+  auto actual = exec(path, "Bar");
 
+  EXPECT_THAT(actual, Contains(std::string{"Bar"}));
+}
+TEST(Multiprocess, ReactOnStdinWhenBlub) {
+  const char *path{"./threads/user_input --stdin2stdout"};
+  auto actual = exec(path, "Blub");
+
+  EXPECT_THAT(actual, Contains(std::string{"Blub"}));
+}
 } // namespace
--- threads/user_input.cpp
+++ threads/user_input.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
-int main(int, char **) {
-  std::cout << "Foo" << '\n';
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+int main(int argc, char **argv) {
+  po::options_description desc("Allowed options");
+  desc.add_options()("help", "produce help message")(
+      "stdin2stdout", "Get line from stdin and pass it to stdout");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("stdin2stdout")) {
+    while (true) {
+      std::string line;
+      std::getline(std::cin, line);
+      std::cout << line << '\n';
+    }
+  } else {
+    std::cout << "Foo" << '\n';
+  }
+  while (true) {
+    // Wait for terminate
+    std::string line;
+    std::getline(std::cin, line);
+  }
   return 0;
 }
