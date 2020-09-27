a4d6b6a6ab9463436982a64b862a6637bce085b4
martin <martingerwallner@gmx.de>
2020-09-27 12:11:15 +0200

    Dirty solution for kings banquet with int32_t

--- kings_banquet/spec/spec.cpp
+++ kings_banquet/spec/spec.cpp
@@ -1,6 +1,7 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+#include "graph.h"
 #include <numeric>
 
 namespace {
@@ -29,14 +30,16 @@ bool IsAFibonacciNumber(std::int32_t const n) {
 
 class KingsTable {
 public:
-  KingsTable(std::int32_t const number_of_seats) : nights_(number_of_seats) {
-    std::iota(nights_.begin(), nights_.end(), 1);
+  KingsTable(std::int32_t const number_of_seats) : knights_(number_of_seats) {
+    std::iota(knights_.begin(), knights_.end(), 1);
   }
   std::int32_t NthKnightLeftToTheKing(std::uint32_t const n) {
-    if (n > nights_.size()) {
+    if (n > knights_.size()) {
       throw std::runtime_error{"Not enough seats"};
     }
     auto neighbors = FindPossibleNeighbors();
+
+    auto graph = BuildGraphFromNeighbors(neighbors);
     for (auto const &n : neighbors) {
       std::cerr << "Possible neighbors of: " << n.first << " are:";
       for (auto const i : n.second) {
@@ -44,18 +47,49 @@ public:
       }
       std::cerr << '\n';
     }
-    return 7;
+    auto leafs = graph.Leafs();
+    std::sort(leafs.begin(), leafs.end(),
+              [](Vertex const &lhs, Vertex const &rhs) {
+                return lhs.GetName() < rhs.GetName();
+              });
+    if (leafs.size() != 2) {
+      std::cerr << "Leafs:";
+      for (auto const &l : leafs) {
+        std::cerr << " " << l.GetName();
+      }
+      std::cerr << '\n';
+      throw std::runtime_error("To many knights need to be beside the king");
+    }
+    auto seating_order =
+        graph.GetPathFromTo(leafs.at(0).GetName(), leafs.at(1).GetName());
+    return seating_order[n - 1];
   }
 
 private:
+  Graph BuildGraphFromNeighbors(
+      std::map<std::int32_t, std::vector<std::int32_t>> &neighbors) {
+    for (std::int32_t i = 1; i <= static_cast<std::int32_t>(knights_.size());
+         ++i) {
+      for (auto &e : neighbors[i]) {
+        if (e > i) {
+          neighbors[e].push_back(i);
+        }
+      }
+    }
+    Graph graph{};
+    for (auto const &vertex : neighbors) {
+      graph.AddVertex(Vertex{vertex.first, vertex.second});
+    }
+    return graph;
+  }
   std::map<std::int32_t, std::vector<std::int32_t>> FindPossibleNeighbors() {
     // Create pairs/s
-    if (nights_.size() < 2) {
+    if (knights_.size() < 2) {
       return {};
     }
     std::map<std::int32_t, std::vector<std::int32_t>> neighbors{};
-    auto first = nights_.cbegin();
-    auto const end = nights_.cend();
+    auto first = knights_.cbegin();
+    auto const end = knights_.cend();
     for (; first != std::prev(end); ++first) {
       std::vector<std::int32_t> neighbors_of_first{};
       auto second = std::next(first);
@@ -73,24 +107,24 @@ private:
   }
 
 private:
-  std::vector<std::int32_t> nights_;
+  std::vector<std::int32_t> knights_;
 };
 TEST(KingsTable, NthKnightLeftToTheKing_WhenNotEnoughSeats) {
   KingsTable table{7};
   EXPECT_THROW(table.NthKnightLeftToTheKing(8), std::runtime_error);
 }
-// TEST(KingsTable, Return3thsKnightOnTableWith7Seats) {
-//   KingsTable table{7};
-//   EXPECT_THAT(table.NthKnightLeftToTheKing(3), Eq(7));
-// }
-// TEST(KingsTable, Return4thsKnightOnTableWith7Seats) {
-//   KingsTable table{7};
-//   EXPECT_THAT(table.NthKnightLeftToTheKing(4), Eq(6));
-// }
-// TEST(KingsTable, Return3thsKnightOnTableWith34Seats) {
-//     KingsTable table{34};
-//     EXPECT_THAT(table.NthKnightLeftToTheKing(3), Eq(30));
-// }
+TEST(KingsTable, Return3thsKnightOnTableWith7Seats) {
+  KingsTable table{7};
+  EXPECT_THAT(table.NthKnightLeftToTheKing(3), Eq(7));
+}
+TEST(KingsTable, Return4thsKnightOnTableWith7Seats) {
+  KingsTable table{7};
+  EXPECT_THAT(table.NthKnightLeftToTheKing(4), Eq(6));
+}
+TEST(KingsTable, Return3thsKnightOnTableWith34Seats) {
+  KingsTable table{34};
+  EXPECT_THAT(table.NthKnightLeftToTheKing(3), Eq(30));
+}
 
 TEST(Fibonacci, CalculateUpToAtLeastN) {
   std::vector<std::int32_t> const expected{1, 1, 2, 3, 5, 8, 13};
