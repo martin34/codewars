e638148ad700e9b7590a1c9c3601f5f82eab3bda
martin <martingerwallner@gmx.de>
2021-02-17 21:16:26 +0100

    Transform plot

---
+++ transform/plot.py
@@ -0,0 +1,72 @@
+import csv
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+
+class Colors:
+    colors = {"b", "g", "r", "c", "m", "y", "k"}
+
+    def __init__(self):
+        self.current = cycle(Colors.colors)
+
+    def next(self):
+        return next(self.current)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self, b):
+        res = Point(self.x, self.y)
+        res.x += b.x
+        res.y += b.y
+        return res
+
+    def plot(self, color="b"):
+        plt.plot([self.x], [self.y], marker="o", color=color)
+
+
+class Line:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def plot(self, color="b"):
+        plt.plot([self.a.x, self.b.x], [self.b.y, self.b.y], color=color)
+
+
+class Snap:
+    def __init__(self, point, lines):
+        self.point = point
+        self.lines = lines
+
+    def plot(self, color):
+        self.point.plot(color)
+        for line in self.lines:
+            line.plot(color)
+
+
+with open("data.txt", newline="") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
+
+    snaps = []
+    for row in reader:
+        o = Point(row["x"], row["y"])
+        a = Point(row["ax"], row["ay"])
+        b = Point(row["bx"], row["by"])
+        oa = o.add(a)
+        ob = o.add(b)
+
+        ar = Point(row["arx"], row["ary"])
+        br = Point(row["brx"], row["bry"])
+        oar = o.add(ar)
+        obr = o.add(br)
+
+        snaps.append(Snap(o, [Line(oa, ob), Line(oar, obr)]))
+
+    color = Colors()
+    for snap in snaps:
+        snap.plot(color.next())
+    plt.show()
