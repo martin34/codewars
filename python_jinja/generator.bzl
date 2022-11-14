c1a5e254c4709151af65af695077bb78c4c20fa1
martin <martingerwallner@gmx.de>
2022-11-14 21:12:43 +0100

    Create macro and use it with python jinja

---
+++ python_jinja/generator.bzl
@@ -0,0 +1,8 @@
+def greeting_generator(name, names_file):
+    native.genrule(
+        name = name,
+        outs = ["main.cpp"],
+        srcs = [names_file],
+        cmd = "python3 $(location //python_jinja:generator) $< $@",
+        tools = ["//python_jinja:generator"],
+    )
--- python_jinja/generator.py
+++ python_jinja/generator.py
@@ -2,13 +2,13 @@ from jinja2 import Environment, PackageLoader
 import sys
 
 env = Environment(
-    loader=PackageLoader("generator"), # Python package --> name of this file
+    loader=PackageLoader("generator"),  # Python package --> name of this file
 )
 
 template = env.get_template("example_template.cpp.jinja")
 names = []
-with open(sys.argv[1], 'r') as f:
+with open(sys.argv[1], "r") as f:
     names = [line.rstrip() for line in f]
 
-with open(sys.argv[2], 'w') as f:
+with open(sys.argv[2], "w") as f:
     f.write(template.render(names=names[1:], special=names[0]))
